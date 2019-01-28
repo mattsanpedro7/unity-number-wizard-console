64228ec794af8359860f087d86d2c10713a22980
mattsanpedro7 <mattsanpedro76@gmail.com>
2019-01-27 17:15:13 -0800

    completed number guessing game and fixed max number edge case bug

--- Assets/numberWizard.cs
+++ Assets/numberWizard.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class numberWizard : MonoBehaviour
 {
+    // declare variable and initialize
+    int max = 1000;
+    int min = 0;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-        // declare variable and initilize
-        int max = 1000;
-        int min = 0;
-
         // print("Welcome to number wizard!");
         // gives more flexibility and control
         Debug.Log("Welcome to number wizard!");
@@ -19,6 +20,9 @@ public class numberWizard : MonoBehaviour
         Debug.Log("The lowest number is: " + min);
         Debug.Log("Tell me if your number is higher or greater than 500");
         Debug.Log("Push up = higher, Push down = lower, Push enter = number found");
+
+        // edge case, cannot reach 1000, will stop at max-1 OR 999
+        max = max + 1;
     }
 
     // Update is called once per frame
@@ -28,11 +32,19 @@ public class numberWizard : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.UpArrow))
             {
                 print("Up arrow key was pressed");
+                min = guess;
+                guess = (max + min) / 2;
+                Debug.Log("Guess is: " + guess);
+                Debug.Log("New min is: " + min);
             }
 
             else if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 print("Down arrow key was pressed");
+                max = guess;
+                guess = (max + min) / 2;
+                Debug.Log("Guess is: " + guess);
+                Debug.Log("New max is: " + max);
             }
 
             else if (Input.GetKeyDown(KeyCode.Return)) 
